1401f790a7472a5a2949e5e24e2972636061d003
Eddie Yan <eddiey@nvidia.com>
2025-06-11 23:37:48 +0000

    oops

--- aten/src/ATen/cuda/CUDABlas.cpp
+++ aten/src/ATen/cuda/CUDABlas.cpp
@@ -1681,9 +1681,9 @@ bool gemm_and_bias(
   preference.setAttribute(CUBLASLT_MATMUL_PREF_MIN_ALIGNMENT_D_BYTES, d_alignment);
 #endif
 #ifndef USE_ROCM
-  auto useDdesc = Cdesc.descriptor();
-#else
   auto useDdesc = Ddesc.descriptor();
+#else
+  auto useDdesc = Cdesc.descriptor();
 #endif
 
   cublasLtMatmulHeuristicResult_t heuristicResult = {};
