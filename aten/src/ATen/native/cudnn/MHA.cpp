e9a27ad91c7b9b4d323dd986cd97795c1e5293f2
eqy <eddiey@nvidia.com>
2025-06-16 14:35:06 -0700

    Update MHA.cpp

--- aten/src/ATen/native/cudnn/MHA.cpp
+++ aten/src/ATen/native/cudnn/MHA.cpp
@@ -298,13 +298,13 @@ struct MHAGraphCache {
 
 auto& getMHAGraphCache_() {
   thread_local auto& instance =
-    *new MHAGraphCache<std::shared_ptr<fe::graph::Graph>, MHACacheKeyWrapper>;
+      *new MHAGraphCache<std::shared_ptr<fe::graph::Graph>, MHACacheKeyWrapper>;
   return instance;
 }
 
 auto& getMHAGraphBackwardCache_() {
   thread_local auto& instance =
-    *new MHAGraphCache<std::shared_ptr<fe::graph::Graph>, MHACacheKeyWrapper>;
+      *new MHAGraphCache<std::shared_ptr<fe::graph::Graph>, MHACacheKeyWrapper>;
   return instance;
 }
 
